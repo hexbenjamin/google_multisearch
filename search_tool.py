d7ad8636c4c1e829203ce7b7593e78ce0d8140a3
hexbenjamin <bnjmnadase@gmail.com>
2022-06-08 15:03:30 -0400

    initial commit

---
+++ search_tool.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+def link_substitute(link):
+    out_str = ""
+    esc_char = "%25"
+    for char in link:
+        if char == ":":
+            char == esc_char + "3A"
+        
+        out_str += char
+    
+    return out_str
+
+
+test_link = "https://test.bungled.funk/"
+print(link_substitute(test_link))
