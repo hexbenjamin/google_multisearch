4dfa6969cd26f215839e4ac1886679ea730ac2b9
hexbenjamin <bnjmnadase@gmail.com>
2022-06-08 17:05:05 -0400

    / new: - reads txt file as input list of sites to search - formats those links according to engine needs - created dictionary, in case of other engine support / next: - generate search link from list - input / "ux" dev

--- search_tool.py
+++ search_tool.py
@@ -1,5 +1,12 @@
 import webbrowser
 
+
+engine_dict = {
+    "key": ["search_prefix", "site_specifier", "or_operand"],
+    "google": ["https://www.google.com/search?q=", "+site%253A", "+OR"]
+}
+
+
 def link_substitute(link):
     out_str = ""
     esc_char = "%25"
@@ -13,6 +20,15 @@ def link_substitute(link):
     
     return out_str
 
+def site_list_format(engine: str, fp: str):
+    sites_list = []
+    prefix, add_site, or_str = engine_dict[engine]
+    with open(fp, 'r') as list_file:
+        in_list = list_file.readlines()
+        for site in in_list:
+            formatted = link_substitute(site.strip())
+            sites_list.append(formatted)
+    return sites_list
 
-test_link = "https://test.bungled.funk/"
-print(link_substitute(test_link))
+
+print(site_list_format("google", "sites.txt"))
