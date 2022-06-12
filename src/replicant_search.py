98e594f64d692faf79c4252d58e459f402ce22c8
hexbenjamin <bnjmnadase@gmail.com>
2022-06-12 00:50:10 -0400

    branch ender! [06.12.2022]
    NEW:
    [>] greatly simplified engine selection
        >> previously planned to use a sequence string to lookup link parts
        >> now using a link template w/ split character to divide into parts
    > completed scope.py module & labeled function return formats
    > began overhauling replicant_search.py to implement new stuff
    
    TODO:
    [>] merge branch! set selection is ready to implement!

--- src/replicant_search.py
+++ src/replicant_search.py
@@ -1,100 +1,54 @@
-import os
+# import engines
+import scope
+
 import sys
 import webbrowser
 
-
 engine_dict = {
-    "key": ["search_prefix", "site_specifier", "or_operand"],
-    "google": ["https://www.google.com/search?", "site%3A", "+OR+"]
+    "google": "https://www.google.com/search?q=^+site%3A^+OR+",
+    "google_img": "https://www.google.com/search?tbm=isch&q=^+site%3A^+OR+"
 }
 
-def scope_reader(fp: str):
-    scope_list = []
-    with open(fp, 'r') as list_file:
-        in_list = list_file.readlines()
-        for site in in_list:
-            scope_list.append(site.strip())
-    return scope_list
+def construct_link(query: str, scope_set: int, engine="google"):
+    search_prefix, site_spec, or_str = engine_dict[engine].split('^')
+    
+    # '{search_prefix} + query + {site_spec} + scope0 + {or_str} + {site_spec} + scope1'
 
 
-def link_substitute(link: str):
-    out_str = ""
-    
-    ## determined this to be unnecessary:
-    ## once the search happens, the link is formatted to use a % (%25) escape
-    ## if the escape characters are included in the input link, the colons & slashes are escaped in the actual search
-    ## (maybe let's just let google do as google does today)
-    
-    # esc_char = "%25"
-    
-    for char in link:
-        if char == ":":
-            char = "%3A"
-        elif char == "/":
-            char = "%2F"
-
-        out_str += char
-    
-    return out_str
-
-def scope_format(fp: str):
-    in_list = scope_reader(fp)
-    scope_list = []
-    for site in in_list:
-            formatted = link_substitute(site)
-            scope_list.append(formatted)
-    return scope_list
-
-def construct_link(query: str, scope_file='scope.txt', engine='google', mode='std'):
-    prefix, add_site, or_str = engine_dict[engine]
-    
-    if mode == 'img':
-        prefix += "tbm=isch&"
-
-    scope = scope_format(scope_file)
-
-    validated_query = ""
-    for letter in query:
-        if letter == " ":
-            letter = "+"
-        validated_query += letter
-    validated_query = "q=" + validated_query
-    
-    out_link = prefix + validated_query + "+"
-    
-    for i in range(len(scope)):
-        current_link = scope[i]
-        if len(scope) - i == 1:
-            out_link += add_site + current_link
-        else:
-            out_link += add_site + current_link + or_str
-    
-    return out_link
 
 def main():
     print("welcome to HEX BENJAMIN's multi_search.")
+    
+    scope_list = scope.reader('scope.txt')
+    engine_list = list(engine_dict.keys())
 
-    # print("what search engine would you like to use for the operation?")
-    # engine = input('>>')
+    ### ENGINE SELECT
+    print("please select a search engine to utilize from the following list: ")
+    
+    options = ""
+    for i in range(len(engine_list)):
+        entry = f'{i}={engine_list[i]}'
+        if i != len(engine_list) - 1:
+            entry += ' / '
+        
+        options += entry
+    
+    engine = input(f'[{options}]')
 
-    engine = "google"
+    
 
-    print("multi_search will load scope links from 'scope.txt' by default.")
-    print("press [Enter] to continue, or specify the path to a different .txt file.")
-    custom_scope = input('>> ')
-
+    ### QUERY SELECT
     print("now, what are we searching for? input your query below.")
     user_query = input('>> ')
 
-    if custom_scope != "":
-        output = construct_link(user_query, custom_scope)
-    else:
-        output = construct_link(user_query)
 
-    input(f"press [Enter] to start a search for '{user_query}'.")
 
-    webbrowser.open(output)
-    sys.exit(0)
+    # webbrowser.open(output)
+    # sys.exit(0)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+# print(construct_link('https://thangs.com/', 'google_img'))
+
+main()
--- src/scope.py
+++ src/scope.py
@@ -1,33 +1,4 @@
-from posixpath import split
 import re
-
-
-engine_dict = {
-    "key": ["sequence", "i : imgs_str", "o : or_str", "P: prefix", "Q : q_prefix", "S : site_spec"],
-    0: ["PiQSo", "tbm=isch&", "+OR+", "https://www.google.com/search?", "q=", "+site%3A"]
-}
-
-# print(engine_dict[0][0]) // print engine name, e.g.
-
-def set_format(input_set: list):
-    formatted_list = []
-    for link in input_set:
-        out_str = ""
-    
-        ## determined this to be unnecessary:
-        # esc_char = "%25"
-        
-        for char in link:
-            if char == ":":
-                char = "%3A"
-            elif char == "/":
-                char = "%2F"
-
-            out_str += char
-        
-        formatted_list.append(out_str)
-    
-    return formatted_list
 
 
 def reader(fp: str):
@@ -62,10 +33,34 @@ def set_select(split_list: list, set_id: int):
     return set_list
 
 
-split_list = reader('scope.txt')
+def set_format(input_set: list):
+    # RETURNS:
+    # LIST of *FORMATTED* STR links
+    
+    formatted_list = []
+    for link in input_set:
+        out_str = ""
+    
+        ## determined this to be unnecessary:
+        # esc_char = "%25"
+        
+        for char in link:
+            if char == ":":
+                char = "%3A"
+            elif char == "/":
+                char = "%2F"
 
-selected = set_select(split_list, 1)
-print(selected)
+            out_str += char
+        
+        formatted_list.append(out_str)
+    
+    return formatted_list
 
-formatted = set_format(selected)
-print(formatted)
+
+# split_list = reader('scope.txt')
+
+# selected = set_select(split_list, 1)
+# print(selected)
+
+# formatted = set_format(selected)
+# print(formatted)
